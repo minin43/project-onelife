b1b5263216e5a2efdc7da1d629b4cdaced4e6262
Logan <lobsterlogan43@yahoo.com>
2016-11-09 19:47:42 -0600

    bombs

--- gamemode/sv_bombs.lua
+++ gamemode/sv_bombs.lua
@@ -1,18 +1,78 @@
 print( "sv_bombs initialization..." )
-bombspawn = bombspawn or {}
+hook.Add( "GameStart", "SetBombs", function( mode )
+	objspawn = objspawn or { }
 
--- Pasting this in console gets everyone's current location: lua_run for k, v in pairs(player.GetAll()) do print(v, v:GetPos())end useful for getting good spawn positions
+	-- Pasting this in console gets everyone's current location: lua_run for k, v in pairs(player.GetAll()) do print(v, v:GetPos())end useful for getting good spawn positions
 
-bombspawn[ "gm_construct" ] = {
-	{ "L", Vector( -2402, -1560, -143 ), 300, 0 },
-	{ "O", Vector( -2250, -2786, 256 ), 400, 0 },
-	{ "G", Vector( -845, 564, -160 ), 200, 0 },
-	{ "A", Vector( -4531, 5393, -95 ), 400, 0 },
-	{ "N", Vector( 1399, -1659, -143 ), 300, 0 },
-	{ "!", Vector( 773, 4239, -31 ), 50, 0 }
-}
+	objspawn[ "hotpoint" ] = {
+		[ "gm_devruins" ] = {
+			--//Vector positions, hotpoint size
+			{ Vector( 0, 0, 0 ), 200 }
+		}
+		[ "de_crash" ] = {
+			{ Vector( 0, 0, 0 ), 200 },
+			{ Vector( 0, 0, 0 ), 200 }
+		}
+		[ "de_keystone_beta" ] = {
+			{ Vector( 0, 0, 0 ), 200 },
+			{ Vector( 0, 0, 0 ), 200 }
+		}
+		[ "de_secretcamp" ] = {
+			{ Vector( 0, 0, 0 ), 200 }
+		}
+		[ "ttt_bf3_scrapmetal" ] = {
+			{ Vector( 0, 0, 0 ), 200 },
+			{ Vector( 0, 0, 0 ), 200 }
+		}
+	}
 
-if not bombspawn[ game.GetMap() ] then
-	return
---else
-end
+	objspawn[ "cache" ] = {
+		[ "gm_devruins" ] = {
+			[ "A" ] = { }
+		}
+		[ "de_crash" ] = {
+			[ "A" ] = { }
+			[ "B" ] = { }
+		}
+		[ "de_keystone_beta" ] = {
+			[ "A" ] = { }
+			[ "B" ] = { }
+		}
+		[ "de_secretcamp" ] = {
+			[ "A" ] = { }
+			[ "B" ] = { }
+		}
+		[ "ttt_bf3_scrapmetal" ] = {
+			[ "A" ] = { }
+		}
+	}
+
+	objspawn[ "hq" ] = {
+		[ "gm_devruins" ] = {
+			[ 1 ] = { }
+			[ 2 ] = { }
+		}
+		[ "de_crash" ] = {
+			[ 1 ] = { }
+			[ 2 ] = { }
+		}
+		[ "de_keystone_beta" ] = {
+			[ 1 ] = { }
+			[ 2 ] = { }
+		}
+		[ "de_secretcamp" ] = {
+			[ 1 ] = { }
+			[ 2 ] = { }
+		}
+		[ "ttt_bf3_scrapmetal" ] = {
+			[ 1 ] = { }
+			[ 2 ] = { }
+		}
+	}
+
+	if not objspawn[ mode ][ game.GetMap() ] then
+		return
+	--else
+	
+	end
+end )
